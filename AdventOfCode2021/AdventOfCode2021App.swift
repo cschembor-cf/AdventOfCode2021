545eda2426b3998eabd773e890cd932b47f2b8aa
Connor <connor.schembor@cardflight.com>
2021-11-30 21:32:40 -0500

    Initial Commit

---
+++ AdventOfCode2021/AdventOfCode2021App.swift
@@ -0,0 +1,17 @@
+//
+//  AdventOfCode2021App.swift
+//  AdventOfCode2021
+//
+//  Created by Connor Schembor on 11/30/21.
+//
+
+import SwiftUI
+
+@main
+struct AdventOfCode2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdventOfCode2021/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AdventOfCode2021
+//
+//  Created by Connor Schembor on 11/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
