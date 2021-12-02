12f7ffe7d61f43fec556b58aa3bd9153977c4f2e
Connor <connor.schembor@cardflight.com>
2021-12-02 14:21:04 -0500

    Setup day 3, remove ui tests

---
+++ AdventOfCode2021/Challenges/Day3/Day3Challenge.swift
@@ -0,0 +1,12 @@
+//
+//  Day3Challenge.swift
+//  AdventOfCode2021
+//
+//  Created by Connor Schembor on 12/2/21.
+//
+
+import Foundation
+
+class Day3Challenge: Challenge {
+    
+}
---
+++ AdventOfCode2021/Challenges/Day3/Day3View.swift
@@ -0,0 +1,23 @@
+//
+//  Day3View.swift
+//  AdventOfCode2021
+//
+//  Created by Connor Schembor on 12/2/21.
+//
+
+import SwiftUI
+
+struct Day3View: View {
+    @ObservedObject private var challenge = Day3Challenge()
+
+    var body: some View {
+
+        DayView(dayNumber: 2, challenge: challenge)
+    }
+}
+
+struct Day3View_Previews: PreviewProvider {
+    static var previews: some View {
+        Day3View()
+    }
+}
