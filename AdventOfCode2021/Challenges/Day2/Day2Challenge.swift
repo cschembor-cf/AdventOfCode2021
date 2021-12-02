25cfe7f1dbfaf0110636e68890b8a33de0884913
Connor <connor.schembor@cardflight.com>
2021-12-02 09:24:12 -0500

    day 2 part 2

--- AdventOfCode2021/Challenges/Day2/Day2Challenge.swift
+++ AdventOfCode2021/Challenges/Day2/Day2Challenge.swift
@@ -9,6 +9,15 @@ import Foundation
 
 class Day2Challenge: Challenge {
 
+    var testInput = """
+        forward 5
+        down 5
+        forward 8
+        up 3
+        down 8
+        forward 2
+"""
+
     override func part1() {
         var horizontalPos = 0
         var depth = 0
@@ -19,6 +28,29 @@ class Day2Challenge: Challenge {
                 let posUpdate = command.getFinalPos()
                 horizontalPos += posUpdate.horizontalPos
                 depth += posUpdate.depth
+            }
+        }
+
+        answer = "\(horizontalPos * depth)"
+    }
+
+    override func part2() {
+        var horizontalPos = 0
+        var depth = 0
+        var aim = 0
+        let lines = Utils.readFile("day2_input")
+        for line in lines {
+            let inputs = line.split(separator: " ")
+            if let command = PosCommand(dir: String(inputs[0]), amount: Int(inputs[1])!) {
+                switch (command) {
+                case .down(let amount):
+                    aim += amount
+                case .up(let amount):
+                    aim -= amount
+                case .forward(let amount):
+                    horizontalPos += amount
+                    depth += aim * amount
+                }
             }
         }
 
