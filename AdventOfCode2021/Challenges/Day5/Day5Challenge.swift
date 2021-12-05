e60112ed187b19c524a754221edf62fa75851a50
Connor <connor.schembor@cardflight.com>
2021-12-05 14:38:09 -0500

    Day 5 part 2

--- AdventOfCode2021/Challenges/Day5/Day5Challenge.swift
+++ AdventOfCode2021/Challenges/Day5/Day5Challenge.swift
@@ -8,6 +8,10 @@
 import Foundation
 
 class Day5Challenge: Challenge {
+    private var numOverlappingCoordinates = 0
+    private var coordinatesVisited = Set<Coordinate>()
+    private var coordinatesOverlapped = Set<Coordinate>()
+
     let testInput = """
         0,9 -> 5,9
         8,0 -> 0,8
@@ -22,31 +26,51 @@ class Day5Challenge: Challenge {
     """
 
     override func part1() {
-        var overlappingCoordinates = 0
-        var coordinatesVisited = Set<Coordinate>()
-        var coordinatesOverlapped = Set<Coordinate>()
+        reset()
         let lines = parseInput()
         for line in lines {
-            let horizontal = line.start.y == line.end.y
-            let vertical = line.start.x == line.end.x
-            guard horizontal || vertical else { continue }
+            guard line.isHorizontal || line.isVertical else { continue }
             for coordinate in line.coordinates {
                 if coordinatesVisited.contains(coordinate) {
                     guard !coordinatesOverlapped.contains(coordinate) else { continue }
                     coordinatesOverlapped.insert(coordinate)
-                    overlappingCoordinates += 1
+                    numOverlappingCoordinates += 1
                 } else {
                     coordinatesVisited.insert(coordinate)
                 }
             }
         }
 
-        answer = "\(overlappingCoordinates)"
+        answer = "\(numOverlappingCoordinates)"
+    }
+
+    override func part2() {
+        reset()
+        let lines = parseInput()
+        for line in lines {
+            guard line.isHorizontal || line.isVertical || line.isDiagonal else { continue }
+            for coordinate in line.coordinates {
+                if coordinatesVisited.contains(coordinate) {
+                    guard !coordinatesOverlapped.contains(coordinate) else { continue }
+                    coordinatesOverlapped.insert(coordinate)
+                    numOverlappingCoordinates += 1
+                } else {
+                    coordinatesVisited.insert(coordinate)
+                }
+            }
+        }
+
+        answer = "\(numOverlappingCoordinates)"
+    }
+
+    private func reset() {
+        self.numOverlappingCoordinates = 0
+        self.coordinatesVisited.removeAll()
+        self.coordinatesOverlapped.removeAll()
     }
 
     private func parseInput() -> [Line] {
-        let lines = Utils.readFile("day5_input")//testInput.split(whereSeparator: \.isNewline).map { $0.trimmingCharacters(in: .whitespaces) } // Utils.readFile("day5_input")
-
+        let lines = Utils.readFile("day5_input")
         var output: [Line] = []
         for line in lines {
             let coords: [Coordinate] = line.components(separatedBy: "->")
@@ -64,6 +88,20 @@ class Day5Challenge: Challenge {
 fileprivate struct Line {
     let start: Coordinate
     let end: Coordinate
+
+    var direction: Direction {
+        if self.isDiagonal { return .diagonal }
+        if self.isHorizontal { return .horizontal }
+        if self.isVertical { return .vertical }
+        return .unknown
+    }
+
+    enum Direction {
+        case diagonal
+        case horizontal
+        case vertical
+        case unknown
+    }
 }
 
 fileprivate struct Coordinate: Equatable, Hashable {
@@ -74,9 +112,39 @@ fileprivate struct Coordinate: Equatable, Hashable {
 fileprivate extension Line {
     var coordinates: [Coordinate] {
         var _coords: [Coordinate] = []
-        let horizontal = self.start.y == self.end.y
-        let vertical = self.start.x == self.end.x
-        if horizontal {
+        switch self.direction {
+        case.diagonal:
+            let horizontalPositive = self.start.x < self.end.x
+            let verticalPositive = self.start.y < self.end.y
+            switch (horizontalPositive, verticalPositive) {
+            case (true, true):
+                var y = self.start.y
+                for i in self.start.x...self.end.x {
+                    _coords.append(.init(x: i, y: y))
+                    y += 1
+                }
+            case (true, false):
+                var y = self.start.y
+                for i in self.start.x...self.end.x {
+                    _coords.append(.init(x: i, y: y))
+                    y -= 1
+                }
+            case (false, true):
+                var y = self.end.y
+                for i in self.end.x...self.start.x {
+                    _coords.append(.init(x: i, y: y))
+                    y -= 1
+                }
+            case (false, false):
+                var y = self.end.y
+                for i in self.end.x...self.start.x {
+                    _coords.append(.init(x: i, y: y))
+                    y += 1
+                }
+            }
+            break
+
+        case .horizontal:
             let y = self.start.y
             let (start, end) = self.start.x < self.end.x
             ? (self.start.x, self.end.x)
@@ -85,7 +153,8 @@ fileprivate extension Line {
             for i in start...end {
                 _coords.append(.init(x: i, y: y))
             }
-        } else if vertical {
+
+        case .vertical:
             let x = self.start.x
             let (start, end) = self.start.y < self.end.y
             ? (self.start.y, self.end.y)
@@ -94,8 +163,22 @@ fileprivate extension Line {
             for j in start...end {
                 _coords.append(.init(x: x, y: j))
             }
+
+        default: break
         }
 
         return _coords
+    }
+
+    var isVertical: Bool {
+        self.start.x == self.end.x
+    }
+
+    var isHorizontal: Bool {
+        self.start.y == self.end.y
+    }
+
+    var isDiagonal: Bool {
+        return abs(self.end.y - self.start.y) == abs(self.end.x - self.start.x)
     }
 }
